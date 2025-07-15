fc7cb63ca31aaed6e0e7658be37da3113fa548a8
Emre Atasavun <120120633+Fridgemann@users.noreply.github.com>
2025-07-15 15:42:32 +0300

    modifications for google compatibility

--- portfolio-site/pages/_document.js
+++ portfolio-site/pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <title>Emre Atasavun - Fullstack Developer</title>
         <meta name="description" content="Portfolio website of Emre Atasavun, an aspiring full-stack developer." />
+        <meta name="google-site-verification" content="imR-XgAaKN6jqTjTVRkDYIj4bUy47COs8ZQCQ5UXQEM" />
         <link rel="icon" href="favicon.svg" />
       </Head>
       <body className="antialiased">
