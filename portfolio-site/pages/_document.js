8405ec481b4392a5dc6aaf7dda0baefac62264de
Emre Atasavun <120120633+Fridgemann@users.noreply.github.com>
2025-07-15 12:29:01 +0300

    updated page title and added meta tag

--- portfolio-site/pages/_document.js
+++ portfolio-site/pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Emre Atasavun - Fullstack Developer</title>
+        <meta name="description" content="Portfolio website of Emre Atasavun, an aspiring full-stack developer." />
         <link rel="icon" href="favicon.svg" />
       </Head>
       <body className="antialiased">
