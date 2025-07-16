a24297fced30b6fefbf43736a3b0f15c6a2f839d
Emre Atasavun <120120633+Fridgemann@users.noreply.github.com>
2025-07-16 22:04:41 +0300

    site metadata & title update
    carried metadata and title from _document.js to _app.js

--- portfolio-site/pages/_app.js
+++ portfolio-site/pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+  <Head>
+    <title>Emre Atasavun - Fullstack Developer</title>
+    <meta name="description" content="Portfolio website of Emre Atasavun, an aspiring full-stack developer." />
+    <meta name="google-site-verification" content="imR-XgAaKN6jqTjTVRkDYIj4bUy47COs8ZQCQ5UXQEM" />
+    <link rel="icon" href="favicon.svg" type="image/svg+xml"/>
+  </Head>
+  
+  <Component {...pageProps} />
+  </>
+  );
 }
--- portfolio-site/pages/_document.js
+++ portfolio-site/pages/_document.js
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Emre Atasavun - Fullstack Developer</title>
-        <meta name="description" content="Portfolio website of Emre Atasavun, an aspiring full-stack developer." />
-        <meta name="google-site-verification" content="imR-XgAaKN6jqTjTVRkDYIj4bUy47COs8ZQCQ5UXQEM" />
-        <link rel="icon" href="favicon.svg" />
       </Head>
       <body className="antialiased">
         <Main />
